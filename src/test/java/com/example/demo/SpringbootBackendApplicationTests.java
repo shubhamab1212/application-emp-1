c1d0b8c410f19ea760d8aeff587edbc77031f9e7
snehal <s.d.khuspe@gmail.com>
2021-09-17 10:01:06 +0530

    test case added

--- src/test/java/com/example/demo/SpringbootBackendApplicationTests.java
+++ src/test/java/com/example/demo/SpringbootBackendApplicationTests.java
@@ -1,13 +1,46 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
 
 @SpringBootTest
 class SpringbootBackendApplicationTests {
 
+	/*
 	@Test
 	void contextLoads() {
 	}
+    */
+	
+	@Autowired
+	EmployeeRepository repo;
+	
+	@Test
+	public void testCreate () {
+		
+		Employee e = new Employee();
+		e.setId(1L);
+		e.setFirstName("rita");
+		e.setLastName("chavan");
+		e.setEmailId("rita@gmail.com");
+		repo.save(e);
+		assertNotNull(repo.findById(1L).get());
+		
+		
+	}
 
+	private void assertNotNull(Employee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+	
 }
