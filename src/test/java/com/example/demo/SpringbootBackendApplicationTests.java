b67be217d2e59a085a6dea0b2a586590be4c646c
snehal <s.d.khuspe@gmail.com>
2021-09-17 11:01:35 +0530

    added all test cases

--- src/test/java/com/example/demo/SpringbootBackendApplicationTests.java
+++ src/test/java/com/example/demo/SpringbootBackendApplicationTests.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,9 +30,9 @@ class SpringbootBackendApplicationTests {
 		
 		Employee e = new Employee();
 		e.setId(1L);
-		e.setFirstName("rita");
-		e.setLastName("chavan");
-		e.setEmailId("rita@gmail.com");
+		e.setFirstName("snehalll");
+		e.setLastName("chavannnn");
+		e.setEmailId("snehallllll@gmail.com");
 		repo.save(e);
 		assertNotNull(repo.findById(1L).get());
 		
@@ -37,6 +43,58 @@ class SpringbootBackendApplicationTests {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	
+	@Test
+	public void testReadAll () {
+		
+		List<Employee> list = repo.findAll();
+		assertThat(list).size().isGreaterThan(0);
+		
+		
+	}
+	
+	@Test
+	public void testSingleProduct () {
+	Employee employee = repo.findById(1L).get();
+	assertEquals("rita", employee.getFirstName());	
+		
+		
+		
+	}
+
+	private void assertEquals(String string, String firstName) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Test
+	public void testUpdate() {
+		Employee e = repo.findById(1L).get();
+		e.setFirstName("shitallll");
+		repo.save(e);
+		assertNotEquals("shamli", repo.findById(1L).get().getFirstName());
+		
+		
+	}
+
+	private void assertNotEquals(String string, String firstName) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	@Test
+	public void testDelete () {
+		
+		repo.deleteById(14L);
+		assertThat(repo.existsById(14L)).isFalse();
+	}
+	
+	
+	
+	
+	
 	
 	
 	
